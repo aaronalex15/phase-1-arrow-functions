007acd2f62bfa15fc6c091f6f881d6722af5362c
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-03-14 18:42:15 -0700

    code complete

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = () => {
+    let a = 2000
+    let b = 100
+    return a / b
+} 
+
+const square = (num) => {
+return num * num
+}
+
+const add = (sum , gum) => {
+    return sum + gum
+}
